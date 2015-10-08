0d441045d4afc8bf1a8d0ebb1ba19562545ed792
Sakhile Masoka <sakhile.harvey@gmail.com>
2015-10-08 14:17:35 +0200

    First Commit with build and check-build scripts

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+#SOURCE_FILE=$NAME-$VERSION.tar.gz
+# We will build the code from the github repo, but if we want specific versions,
+# a new Jenkins job will be created for the version number and we'll provide
+# the URL to the tarball in the configuration.
+SOURCE_REPO="http://download.sourceforge.net/libpng/"
+# We pretend that the $SOURCE_FILE is there, even though it's actually a dir.
+NAME="libpng"
+VERSION="1.6.18"
+SOURCE_FILE="$NAME-$VERSION.tar.gz"
+
+module load ci
+module load gcc/$GCC_VERSION
+
+
+echo "REPO_DIR is "
+echo $REPO_DIR
+echo "SRC_DIR is "
+echo $SRC_DIR
+echo "WORKSPACE is "
+echo $WORKSPACE
+echo "SOFT_DIR is"
+echo $SOFT_DIR
+
+mkdir -p $WORKSPACE
+mkdir -p $SRC_DIR
+mkdir -p $SOFT_DIR
+
+#  Download the source file
+
+if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
+  echo "seems like this is the first build - Let's get the $SOURCE_FILE from $SOURCE_REPO and unarchive to $WORKSPACE"
+  mkdir -p $SRC_DIR
+  wget $SOURCE_REPO/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
+else
+  echo "continuing from previous builds, using source at " $SRC_DIR/$SOURCE_FILE
+fi
+
+tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+cd $WORKSPACE/$NAME-$VERSION
+
+echo "Configuring the build"
+CC=`which gcc` ./configure --prefix=${SOFT_DIR}
+echo "Running the build"
+make all
---
+++ check-build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+module load ci
+echo "About to make the modules"
+cd $WORKSPACE/$NAME-$VERSION
+ls
+echo $?
+
+echo "Run Make Check"
+make check
+
+echo "Run make Install"
+make install
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+   puts stderr "\tAdds $NAME $VERSION to your environment"
+}
+
+module load gcc/$GCC_VERSION
+
+module-whatis   "$NAME $VERSION."
+setenv       LIBPNG_VERSION       $VERSION
+setenv       LIBPNG_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+
+prepend-path 	PATH            $::env(LIBPNG_DIR)/bin
+prepend-path    PATH            $::env(LIBPNG_DIR)/include
+prepend-path    PATH            $::env(LIBPNG_DIR)/bin
+prepend-path    MANPATH         $::env(LIBPNG_DIR)/man
+prepend-path    LD_LIBRARY_PATH $::env(LIBPNG_DIR)/lib
+MODULE_FILE
+) > modules/$VERSION-gcc-$GCC_VERSION
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION-gcc-$GCC_VERSION $LIBRARIES_MODULES/$NAME/$VERSION-gcc-$VERSION
+
+# Testing module
+module avail
+module list
+module load $NAME/$VERSION-gcc-$GCC_VERSION
+# Testing installation
+
+
