c711d716f2ab418dfd60135f63632448258133ab
Bruce Becker <brucellino@gmail.com>
2017-05-03 19:06:14 +0200

    Minor Release Update
      1. Add license text to all scripts
      1. Add functional tests to check build.
      1. Update module paths for new containers

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 . /etc/profile.d/modules.sh
 # We will build the code from the github repo, but if we want specific versions,
 # a new Jenkins job will be created for the version number and we'll provide
--- check-build.sh
+++ check-build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 . /etc/profile.d/modules.sh
 module load ci
 module add zlib
@@ -33,11 +47,13 @@ prepend-path    PATH            $::env(LIBPNG_DIR)/bin
 prepend-path    MANPATH         $::env(LIBPNG_DIR)/man
 prepend-path    LD_LIBRARY_PATH $::env(LIBPNG_DIR)/lib
 MODULE_FILE
-) > modules/$VERSION
-mkdir -p $LIBRARIES_MODULES/$NAME
-cp modules/${VERSION} ${LIBRARIES_MODULES}/${NAME}/${VERSION}
+) > modules/${VERSION}
+mkdir -p ${LIBRARIES}/${NAME}
+cp modules/${VERSION} ${LIBRARIES}/${NAME}/${VERSION}
 
 # Testing module
 module avail
 module list
 module add ${NAME}/${VERSION}
+which libpng16-config
+libpng16-config --version
